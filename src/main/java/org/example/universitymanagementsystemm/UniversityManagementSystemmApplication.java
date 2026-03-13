122aa747926edad348e4d25f0ebc180648e55b12
Memmdeova04 <62lamiye62@gmail.com>
2026-03-13 15:47:47 +0400

    first commit

---
+++ src/main/java/org/example/universitymanagementsystemm/UniversityManagementSystemmApplication.java
@@ -0,0 +1,13 @@
+package org.example.universitymanagementsystemm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityManagementSystemmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityManagementSystemmApplication.class, args);
+    }
+
+}
